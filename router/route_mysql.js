959b374024a41deb06bee4a461fddadcdf4ef145
lintangwisesa <lintangwisesa@ymail.com>
2019-01-29 12:06:28 +0700

    dci back end

--- router/route_mysql.js
+++ router/route_mysql.js
@@ -133,97 +133,23 @@ router.get('/depts', (req, res)=>{
     })
 })
 
+// get all users with its depts
+router.get('/userDept', (req, res)=>{
+    var dbStat = `select users.id, nama, password, users.dept, fulldept, dibuat, diupdate from users, departments where users.dept = departments.dept`
+    db.query(dbStat, (error, output) => {
+        if(error){
+            console.log(error)
+            res.send(error)
+        } else {
+            if (output){
+                var users = output
+                console.log(users)
+                res.send(users)
+            } else {
+                res.send({status: 'error'})
+            }
+        }
+    })
+})
+
 module.exports = router
-
-
-// // GET all data
-// router.get('/data', (req, res) => {
-//     var dbStat = 'select * from products'
-//     db.query(dbStat, (error, output) => {
-//         if(error){
-//             console.log(error)
-//         } else {
-//             console.log(output)
-//             res.send(output)
-//         }
-//     })
-// })
-
-// // GET data by id
-// router.get('/data/:id', (req, res) => {
-//     var dbStat = 'select * from products where id = ?'
-//     db.query(dbStat, req.params.id, (error, output) => {
-//         if(error){
-//             console.log(error)
-//         } else {
-//             console.log(output)
-//             res.send(output)
-//         }
-//     })
-// })
-
-// // POST data
-// router.post('/data', (req, res)=>{
-//     var dbStat = 'insert into products set ?'
-//     var data = {
-//         product_name: req.body.nama,
-//         price: req.body.harga,
-//         info: req.body.info,
-//         quantity: req.body.jumlah
-//     }
-//     db.query(dbStat, data, (error, output) => {
-//         if(error){
-//             console.log(error)
-//             res.send(error)
-//         } else {
-//             console.log(output)
-//             res.send({
-//                 product_name: req.body.nama,
-//                 price: req.body.harga,
-//                 info: req.body.info,
-//                 quantity: req.body.jumlah,
-//                 status: 'Data terkirim' 
-//             })
-//         }
-//     })
-// })
-
-// // PUT edit data by id
-// router.put('/data/:id', (req, res)=>{
-//     var dbStat = 'update products set ? where id = ?'
-//     var data = {
-//         product_name: req.body.nama,
-//         price: req.body.harga,
-//         info: req.body.info,
-//         quantity: req.body.jumlah
-//     }
-//     db.query(dbStat, [data, req.params.id], (error, output) => {
-//         if(error){
-//             console.log(error)
-//             res.send(error)
-//         } else {
-//             console.log(output)
-//             res.send({
-//                 product_name: req.body.nama,
-//                 price: req.body.harga,
-//                 info: req.body.info,
-//                 quantity: req.body.jumlah,
-//                 status: 'Data terupdate' 
-//             })
-//         }
-//     })
-// })
-
-// // DELETE data by id
-// router.delete('/data/:id', (req, res)=>{
-//     var dbStat = 'delete from products where id = ?'
-//     db.query(dbStat, req.params.id, (error, output) => {
-//         if(error){
-//             console.log(error)
-//         } else {
-//             console.log(output)
-//             res.send(output)
-//         }
-//     })
-// })
-
