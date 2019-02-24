d919f7774e6c35cd8b580fee1aae5fd22c563233
lintangwisesa <lintangwisesa@ymail.com>
2019-02-24 18:24:34 +0700

    dci back

--- router/master.js
+++ router/master.js
@@ -21,7 +21,26 @@ db.connect(()=>{
 
 // get all master karyawan data
 router.get('/master', (req, res)=>{
-    var dbStat = `select nip, nama, j.keterangan as jabatan, d.fulldept as department, stat_peg, telp from master m, jabatan j, departments d where j.jabat = m.jabatan and d.dept = m.dept;`
+    var dbStat = `select nip, nama, j.keterangan as jabatan, d.fulldept as department, s.keterangan as seksi, stat_peg, telp from master m, jabatan j, departments d, seksi s where j.jabat = m.jabatan and d.dept = m.dept and s.jabat = m.seksi;`
+    db.query(dbStat, req.params.id, (error, output) => {
+        if(error){
+            console.log(error)
+            res.send(error)
+        } else {
+            if (output){
+                var users = output
+                console.log(users)
+                res.send(users)
+            } else {
+                res.send({status: 'error'})
+            }
+        }
+    })
+})
+
+// get master karyawan data by NIP
+router.get('/master/:nip', (req, res)=>{
+    var dbStat = `select *, j.keterangan as jabatan, d.fulldept as department, s.keterangan as seksi, stat_peg, telp from master m, jabatan j, departments d, seksi s where j.jabat = m.jabatan and d.dept = m.dept and s.jabat = m.seksi and nip = ${req.params.nip}`
     db.query(dbStat, req.params.id, (error, output) => {
         if(error){
             console.log(error)
